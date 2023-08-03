94205d614c77e9cd47e83b337a6bf502df12dd8b
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-03 17:10:39 +0530

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import speech_recognition as sr
-from google.cloud import translate_v2 as translate
-from st_voice_recorder import voice_recorder
 
 def speech_to_text(language="en"):
     r = sr.Recognizer()
@@ -9,20 +7,13 @@ def speech_to_text(language="en"):
     st.write("Please record your speech using the voice recorder below.")
 
     # Get audio recording from the voice recorder component
-    audio_data = voice_recorder()
+    audio_data = st.record("record_button")
 
     try:
         # Perform speech recognition
-        audio_text = sr.AudioData(audio_data)
-        text = r.recognize_google(audio_text, language=language)
+        text = r.recognize_google(audio_data, language=language)
         st.write("Transcription:")
         st.write(text)
-
-        # Language translation
-        translate_client = translate.Client()
-        translated_text = translate_client.translate(text, target_language="en")
-        st.write("Translation (English):")
-        st.write(translated_text["translatedText"])
 
     except sr.UnknownValueError:
         st.error("Sorry, could not understand audio.")
@@ -47,7 +38,11 @@ def main():
     language = st.selectbox("Select Language", list(language_options.keys()))
     language_code = language_options[language]
 
-    speech_to_text(language_code)
+    # Add a record button to start recording audio
+    record_button = st.button("Start Recording")
+
+    if record_button:
+        speech_to_text(language_code)
 
 if __name__ == "__main__":
     main()
