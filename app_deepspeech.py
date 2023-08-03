a5572f8cd1648c238fb284b02011b3de9fd008fa
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-03 16:28:35 +0530

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import speech_recognition as sr
 
-def speech_to_text(language="en-US"):
+def speech_to_text(language="en"):
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
@@ -15,19 +15,21 @@ def speech_to_text(language="en-US"):
     except sr.UnknownValueError:
         st.error("Sorry, could not understand audio.")
     except sr.RequestError as e:
-        st.error("Error fetching results from Google Speech Recognition service; {0}".format(e))
+        st.error(f"Error fetching results from Google Speech Recognition service: {e}")
+    except Exception as e:
+        st.error(f"Error: {e}")
 
 def main():
     st.title("Speech-to-Text Converter")
     st.write("Select the language and start speaking.")
 
     language_options = {
-        "English (US)": "en-US",
-        "Spanish": "es-ES",
-        "French": "fr-FR",
-        "German": "de-DE",
-        "Italian": "it-IT",
-        "Japanese": "ja-JP",
+        "English": "en",
+        "Spanish": "es",
+        "French": "fr",
+        "German": "de",
+        "Italian": "it",
+        "Japanese": "ja",
     }
 
     language = st.selectbox("Select Language", list(language_options.keys()))
