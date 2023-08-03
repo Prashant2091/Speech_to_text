7bc4e3a41629afefd85225c5200e2e7011680f55
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-03 12:18:24 +0530

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,38 +1,50 @@
 import streamlit as st
-import sounddevice as sd
-import numpy as np
 import speech_recognition as sr
+from googletrans import Translator
 
-def record_audio(duration=5, sample_rate=44100):
-    st.write("Recording...")
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
-    sd.wait()
-    return audio_data
-
-def speech_to_text(language="en-US"):
-    audio_data = record_audio()
-    audio_data = np.squeeze(audio_data)
-    audio_bytes = audio_data.tobytes()
-
+def speech_to_text(language="en"):
     r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        st.write("Speak now...")
+        audio = r.listen(source)
 
     try:
         st.write("Transcription:")
-        text = r.recognize_google(audio_bytes, language=language)
+        text = r.recognize_google(audio, language=language)
         st.write(text)
+
+        # Language translation
+        translator = Translator()
+        translated_text = translator.translate(text, src=language, dest="en")
+        st.write("Translation (English):")
+        st.write(translated_text.text)
+
     except sr.UnknownValueError:
         st.error("Sorry, could not understand audio.")
     except sr.RequestError as e:
-        st.error("Error fetching results from Google Speech Recognition service; {0}".format(e))
+        st.error(f"Error fetching results from Google Speech Recognition service: {e}")
+    except Exception as e:
+        st.error(f"Error: {e}")
 
 def main():
     st.title("Speech-to-Text Converter")
     st.write("Select the language and start speaking.")
 
-    language = st.selectbox("Select Language", ["en-US", "es-ES", "fr-FR", "de-DE", "it-IT", "ja-JP"])
+    language_options = {
+        "English": "en",
+        "Spanish": "es",
+        "French": "fr",
+        "German": "de",
+        "Italian": "it",
+        "Japanese": "ja",
+    }
+
+    language = st.selectbox("Select Language", list(language_options.keys()))
 
     if st.button("Start Recording"):
-        speech_to_text(language)
+        language_code = language_options[language]
+        speech_to_text(language_code)
 
 if __name__ == "__main__":
     main()
