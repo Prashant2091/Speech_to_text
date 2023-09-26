625a0532a1dc40b282340d1dfefc9162ef877040
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-09-26 19:27:16 +0530

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,46 +1,29 @@
 import streamlit as st
-import pyaudio
 import speech_recognition as sr
 
 def speech_to_text(language):
     r = sr.Recognizer()
-
     with sr.Microphone() as source:
-        st.write("Speak something...")
+        st.write("Speak now...")
         audio = r.listen(source)
-
     try:
         text = r.recognize_google(audio, language=language)
-        return text
+        st.success("Text: {}".format(text))
     except sr.UnknownValueError:
-        return "Could not understand audio"
-    except sr.RequestError:
-        return "Could not connect to the service"
+        st.error("Could not understand audio")
+    except sr.RequestError as e:
+        st.error("Could not request results; {}".format(e))
 
 def main():
     st.title("Speech to Text Converter")
-    language_options = {
-    "en-US": "English",
-    "es-ES": "Spanish",
-    "fr-FR": "French",
-    "de-DE": "German",
-    "it-IT": "Italian",
-    "pt-BR": "Portuguese (Brazil)",
-    "ja-JP": "Japanese",
-    "ko-KR": "Korean",
-    "ru-RU": "Russian",
-    "zh-CN": "Chinese (Simplified)",
-    "ar-SA": "Arabic (Saudi Arabia)",
-    "nl-NL": "Dutch",
-    "hi-IN": "Hindi (India)",
-    "sv-SE": "Swedish",
-    "tr-TR": "Turkish"
-}
-
-    language = st.selectbox("Select Language", list(language_options.keys()))
-    if st.button("Convert"):
-        text = speech_to_text(language)
-        st.write("Text:", text)
+    language = st.selectbox("Select Language", ["English", "Spanish", "German", "French", "Italian", "Dutch", 
+                                                "Portuguese", "Russian", "Chinese", "Japanese", "Korean", 
+                                                "Arabic", "Hindi", "Swedish", "Turkish"])
+    language_code = {"English": "en", "Spanish": "es", "German": "de", "French": "fr", "Italian": "it",
+                     "Dutch": "nl", "Portuguese": "pt", "Russian": "ru", "Chinese": "zh-CN", "Japanese": "ja",
+                     "Korean": "ko", "Arabic": "ar", "Hindi": "hi", "Swedish": "sv", "Turkish": "tr"}
+    if st.button("Start Recording"):
+        speech_to_text(language_code[language])
 
 if __name__ == "__main__":
     main()
