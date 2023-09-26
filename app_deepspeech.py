ad197db4fd57ffe1e298a4df84cc3b63436bbf81
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-09-26 17:39:51 +0530

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,56 +1,46 @@
 import streamlit as st
 import speech_recognition as sr
-from audio_recorder_streamlit import audio_recorder
 
-def speech_to_text(language="en"):
+# Set page title and format
+st.set_page_config(page_title="Speech to Text Converter", layout="wide")
+
+# Define function for speech recognition
+def convert_speech_to_text(audio_file):
     r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+        return text
 
-    st.write("Click the 'Start Recording' button and speak.")
-
-    if st.button("Start Recording"):
-        audio_data = record_audio()
-        if audio_data:
-            try:
-                st.write("Transcription:")
-                text = r.recognize_google(audio_data, language=language)
-                st.write(text)
-
-            except sr.UnknownValueError:
-                st.error("Sorry, could not understand audio.")
-            except sr.RequestError as e:
-                st.error(f"Error fetching results from Google Speech Recognition service: {e}")
-            except Exception as e:
-                st.error(f"Error: {e}")
-
-def record_audio():
-    audio_data = None
-    with st.spinner("Recording..."):
-        try:
-            audio_bytes = audio_recorder()
-            if audio_bytes:
-                # st.audio(audio_bytes, format="audio/wav")
-                audio_data = st.audio(audio_bytes, format="audio/wav")
-        except Exception as e:
-            st.error(f"Error during audio recording: {e}")
-
-    return audio_data
-
+# Main app function
 def main():
-    st.title("Speech-to-Text Converter")
-    st.write("Select the language and start speaking.")
+    # Set app title and description
+    st.title("Speech to Text Converter")
+    st.write("Upload an audio file and convert speech to text!")
 
-    language_options = {
-        "English": "en",
-        "Spanish": "es",
-        "French": "fr",
-        "German": "de",
-        "Italian": "it",
-        "Japanese": "ja",
-    }
+    # Audio file upload
+    uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3"])
 
-    language = st.selectbox("Select Language", list(language_options.keys()))
+    if uploaded_file:
+        st.write("File uploaded successfully!")
 
-    speech_to_text(language_options[language])
+        # Convert speech to text
+        text = convert_speech_to_text(uploaded_file)
 
+        # Display converted text
+        st.subheader("Converted Text")
+        st.write(text)
+
+    # Add some space
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+
+    # Footer
+    st.markdown("---")
+    st.write("Created with ❤️ by Your Name")
+
+# Run the app
 if __name__ == "__main__":
     main()
